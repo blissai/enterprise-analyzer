ed9634ee1c632986b27ff8afc5c9719949ec8f89
Michael Sive <mikesive@gmail.com>
2016-02-23 09:34:02 +1000

    add in args checks for lint and stats cli tasks

--- lib/linter/locallinter.rb
+++ lib/linter/locallinter.rb
@@ -6,25 +6,32 @@ class LocalLinter
   def initialize(git_dir, commit, log_prefix, linter_config_path)
     @logger = BlissLogger.new
     @git_dir = git_dir.nil? ? '/repository' : File.expand_path(git_dir)
-    unless File.exist? @git_dir
-      puts 'Directory does not exist.'
-      exit 1
-    end
     @linter_config_path = linter_config_path.nil? ? '/linter.yml' : File.expand_path(linter_config_path)
-    unless File.exist? @linter_config_path
-      puts 'Linter config file does not exist.'
-      exit 1
-    end
-
     @commit = commit
     @name = log_prefix
     @linter = YAML::load_file(@linter_config_path)
     @output_file = '/result.txt'
     @api_key = nil
     @repo_key = nil
+    check_args
   end
 
   def execute
     lint_commit(@commit, @linter, @output_file, false)
+  end
+
+  def check_args
+    valid = true
+    if !File.exist? @git_dir
+      puts 'Directory does not exist.'
+      valid = false
+    elsif !File.exist? @linter_config_path
+      puts 'Linter config file does not exist.'
+      valid = false
+    elsif @commit.nil? || @commit.empty?
+      puts 'Please specify a commit.'
+      valid = false
+    end
+    exit 1 unless valid
   end
 end
--- lib/stats/localstats.rb
+++ lib/stats/localstats.rb
@@ -3,7 +3,7 @@ class LocalStats
   include Stats
 
   def initialize(git_dir, commit, log_prefix,
-                 excluded_dirs, repo_test_files, repo_test_dirs, remove_open_source)
+                 excluded_dirs, repo_test_files, repo_test_dirs, remove_open_source = true)
     @logger = BlissLogger.new
     @commit = commit
     @git_dir = git_dir.nil? ? '/repository' : File.expand_path(git_dir)
@@ -15,14 +15,27 @@ class LocalStats
     end
     @name = log_prefix
     @excluded_dirs = excluded_dirs.split(',') rescue []
-    @repo_test_files = repo_test_files.split(',') rescue []
-    @repo_test_dirs = repo_test_dirs.split(',') rescue []
+    @repo_test_files = repo_test_files.split(',') rescue %w(test)
+    @repo_test_dirs = repo_test_dirs.split(',') rescue %w(test spec)
     @api_key = nil
     @repo_key = nil
     @repo = { 'remove_open_source' => remove_open_source }
+    check_args
   end
 
   def execute
     File.write(@output_file, execute_stats_cmd(@commit, false).to_json)
+  end
+
+  def check_args
+    valid = true
+    if !File.exist? @git_dir
+      puts 'Directory does not exist.'
+      valid = false
+    elsif @commit.nil? || @commit.empty?
+      puts 'Please specify a commit.'
+      valid = false
+    end
+    exit 1 unless valid
   end
 end
