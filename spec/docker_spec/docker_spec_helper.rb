68ff4b31419ccffc000e58020b0e7caa47f4b81e
Michael Sive <mikesive@gmail.com>
2016-04-06 09:53:46 +1000

    use js repo for lizard

--- spec/docker_spec/docker_spec_helper.rb
+++ spec/docker_spec/docker_spec_helper.rb
@@ -204,7 +204,7 @@ module DockerSpecHelper
       linter: "#{@dckr}/linters/lizard.yml",
       result: "#{@dckr}/results/lizard_result.txt",
       expected: "#{@dckr}/expected_results/lizard_result.txt"
-    }.merge(@php_repo)
+    }.merge(@js_repo)
 
     @coffeelint = {
       linter: "#{@dckr}/linters/coffeelint.yml",
