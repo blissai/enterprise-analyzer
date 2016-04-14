e8f34f6d6add20248296a0318c7c2a4449541be0
Michael Sive <mikesive@gmail.com>
2016-04-14 10:09:54 +1000

    exponential backoff for aws uploading

--- lib/util/aws_uploader.rb
+++ lib/util/aws_uploader.rb
@@ -1,11 +1,20 @@
 module AwsUploader
-  def upload_to_aws(bucket, key, content)
-    conn = Faraday.new(url: "http://#{bucket}.s3.amazonaws.com") do |faraday|
-      faraday.request :multipart
-      faraday.adapter :net_http
-    end
-    conn.put("/#{key}", content) do |req|
-      req.headers['x-amz-acl'] = 'bucket-owner-read'
+  def upload_to_aws(bucket, key, content, tried = 0)
+    begin
+      conn = Faraday.new(url: "http://#{bucket}.s3.amazonaws.com") do |faraday|
+        faraday.request :multipart
+        faraday.adapter :net_http
+      end
+      conn.put("/#{key}", content) do |req|
+        req.headers['x-amz-acl'] = 'bucket-owner-read'
+      end
+    rescue Faraday::TimeoutError
+      if tried < 3
+        sleep 2**tried
+        upload_to_aws(bucket, key, content, tried + 1)
+      else
+        puts 'Could not connect to AWS. Connection timed out.'.red
+      end
     end
   end
 end
