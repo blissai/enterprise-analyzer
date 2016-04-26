a27738ca046d7f0bc5c9324e09ef422439c1d8c7
Michael Sive <mikesive@gmail.com>
2016-04-26 12:27:42 +1000

    update jshint reporter

--- json.js
+++ json.js
@@ -1,11 +1,33 @@
 'use strict';
+exports = module.exports = stringify
+exports.getSerialize = serializer
+function stringify(obj, replacer, spaces, cycleReplacer) {
+  return JSON.stringify(obj, serializer(replacer, cycleReplacer), spaces)
+}
+function serializer(replacer, cycleReplacer) {
+  var stack = [], keys = []
+  if (cycleReplacer == null) cycleReplacer = function(key, value) {
+    if (stack[0] === value) return "[Circular ~]"
+    return "[Circular ~." + keys.slice(0, stack.indexOf(value)).join(".") + "]"
+  }
+  return function(key, value) {
+    if (stack.length > 0) {
+      var thisPos = stack.indexOf(this)
+      ~thisPos ? stack.splice(thisPos + 1) : stack.push(this)
+      ~thisPos ? keys.splice(thisPos, Infinity, key) : keys.push(key)
+      if (~stack.indexOf(value)) value = cycleReplacer.call(this, key, value)
+    }
+    else stack.push(value)
+    return replacer == null ? value : replacer.call(this, key, value)
+  }
+}
 module.exports = {
-	reporter: function (result) {
-		result.forEach(function (r) {
-			delete r.error['evidence']
-		})
-		console.log(JSON.stringify({
-			result: result
-		}));
-	}
+    reporter: function (result) {
+        result.forEach(function (r) {
+            delete r.error['evidence']
+        })
+        console.log(stringify({
+            result: result
+        }));
+    }
 };
