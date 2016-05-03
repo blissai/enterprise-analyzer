093ab8ec84d23627eed534d91b1e4b99e30cc06f
Michael Sive <mikesive@gmail.com>
2016-05-04 07:46:04 +1000

    fix rspec expect

--- spec/lib_spec/partitioner_spec.rb
+++ spec/lib_spec/partitioner_spec.rb
@@ -10,7 +10,6 @@ RSpec.describe Partitioner do
     partitioner.create_partitions
     expect(partitioner.partition_dirs.first).not_to eq(@git_dir)
     expect(partitioner.partition_dirs.first).to include('/tmp/parts')
-    expect()
     # expect(partitioner.create_partitions.size).to eq(2)
   end
 
