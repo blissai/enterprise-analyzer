ab95b98a7140fcb9e549d0a18cf80c28556e6330
Michael Sive <mikesive@gmail.com>
2016-05-04 07:36:15 +1000

    check git dir for parts in spec

--- spec/lib_spec/partitioner_spec.rb
+++ spec/lib_spec/partitioner_spec.rb
@@ -3,25 +3,27 @@ RSpec.describe Partitioner do
   before(:all) do
     @logger = BlissLogger.new(nil, nil, 'partitionerspec')
     @git_dir = "#{Dir.pwd}/spec/fixtures/projs/ruby"
-    puts "\n\n#{@git_dir}\n\n"
   end
 
   it 'can create partition files' do
-    partitioner = Partitioner.new(@git_dir, @logger, { 'partitionable' => true, 'max_lines' => 1 }, 100)
-    expect(partitioner.create_partitions.size).to eq(2)
-    # expect(partitioner.partition_dirs.first).to eq(@git_dir)
+    partitioner = Partitioner.new(@git_dir, @logger, { 'partitionable' => true, 'max_lines' => 1 })
+    partitioner.create_partitions
+    expect(partitioner.partition_dirs.first).not.to eq(@git_dir)
+    expect(partitioner.partition_dirs.first).to include('/tmp/parts')
+    expect()
+    # expect(partitioner.create_partitions.size).to eq(2)
   end
 
 
   it 'doesnt create partition files' do
-    partitioner = Partitioner.new(@git_dir, @logger, { 'partitionable' => false, 'max_lines' => 1 })
+    partitioner = Partitioner.new(@git_dir, @logger, 'partitionable' => false, 'max_lines' => 1)
     expect(partitioner.create_partitions.size).to eq(1)
     expect(partitioner.partition_dirs.first).to eq(@git_dir)
   end
 
 
   it 'shouldnt create partition files' do
-    partitioner = Partitioner.new(@git_dir, @logger, { 'partitionable' => true })
+    partitioner = Partitioner.new(@git_dir, @logger, 'partitionable' => true)
     expect(partitioner.create_partitions.size).to eq(1)
     expect(partitioner.partition_dirs.first).to eq(@git_dir)
   end
