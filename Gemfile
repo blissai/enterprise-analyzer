3226eaa987369374aecbc66233fff61b05d6019f
Michael Sive <mikesive@gmail.com>
2016-06-24 11:50:07 +1000

    add rcov

--- Gemfile
+++ Gemfile
@@ -21,4 +21,5 @@ group :test do
   gem 'equivalent-xml'
   gem 'pry'
   gem 'pry-nav'
+  gem 'simplecov', require: false
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,12 @@
 require 'pry'
 require_relative '../lib/bootstrap.rb'
 require_relative './mocks/statstaskmock.rb'
+require 'simplecov'
+
+# save to CircleCI's artifacts directory if we're on CircleCI
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
+  SimpleCov.coverage_dir(dir)
+end
+
+SimpleCov.start
